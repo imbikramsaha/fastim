120c69f6ace4d68b2e9120ac32fa11990cbd306e
Bikram Saha <84061957+imbikramsaha@users.noreply.github.com>
2022-12-09 23:45:29 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fastim",
+    version="0.1.0",
+    author="Bikram Saha",
+    author_email="imbikramsaha@gmail.com",
+    description="fastim helps you to import all the necessary ml and dl libraries by writing 1 line Code.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/imbikramsaha/fastim",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
